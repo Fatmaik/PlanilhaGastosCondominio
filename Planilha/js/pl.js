064eee45728b34ff5a1cc7d473183dd5aaaa2427
unknown <dionathan_bass@hotmail.com>
2017-02-02 15:28:46 -0200

    Versao 2 done

---
+++ Planilha/js/pl.js
@@ -0,0 +1,272 @@
+window.onload = function() {
+    document.getElementById("ap101").onclick = ap101;
+    document.getElementById("ap102").onclick = ap102;
+    document.getElementById("ap201").onclick = ap201;
+    document.getElementById("ap202").onclick = ap202;
+    document.getElementById("ap301").onclick = ap301;
+    document.getElementById("ap302").onclick = ap302;
+    
+}
+function ap101() {
+    var aguaMesAnt101 = document.getElementById("aguaMesAnt101");
+    var AguaMesAtu101 =  document.getElementById("AguaMesAtu101");
+    var mcagua101 =  document.getElementById("mcagua101");
+    var valAguaAp101 = document.getElementById("valAguaAp101");
+    var acrescAgua101 =  document.getElementById("acrescAgua101");
+    var totalAgua101 =  document.getElementById("totalAgua101");
+
+    var gasMesAnt101 = document.getElementById("gasMesAnt101");
+    var gasMesAtu101 = document.getElementById("gasMesAtu101");
+    var mcgas101 =  document.getElementById("mcgas101");
+    var totalGas101 =  document.getElementById("totalGas101");
+
+    var mAnt101 = prompt("leitura mes Anterior Agua 101: ");
+    aguaMesAnt101.innerText =  mAnt101;
+
+    var mAtu101 = prompt("Informe leitura mes Atual Agua 101: ");
+    aguaMesAtu101.innerText = mAtu101;
+
+    var mc101 = prompt("Mc Agua 101: ");
+    mcagua101.innerText = mc101;
+
+    var valAgua101 = prompt("Valor Gasto 101: ");
+    valAguaAp101.innerText = "R$ " + valAgua101;
+
+    var acrescimo101 = prompt("Acrescimo: ");
+    acrescAgua101.innerText = "R$ " + acrescimo101;
+
+    var totA101 = prompt("Total de Agua 101: ");
+    totalAgua101.innerText = "R$ " + totA101;
+
+    var gasMAnt101 = prompt("Leitura anterior gas 101: ");
+    gasMesAnt101.innerText = gasMAnt101;
+
+    var gasMAtu101 = prompt("Leitura atual gas 101: ")
+    gasMesAtu101.innerText = gasMAtu101;
+
+    var gasM101 = prompt("Mc Gas 101: ");
+    mcgas101.innerText = gasM101;
+
+    var totG101 = prompt("Informe Total de gas 101: ");
+    totalGas101.innerText = "R$ " + totG101;
+};
+
+function ap102() {
+    var aguaMesAnt102 = document.getElementById("aguaMesAnt102");
+    var AguaMesAtu102 =  document.getElementById("AguaMesAtu102");
+    var mcagua102 =  document.getElementById("mcagua102");
+    var valAguaAp102 = document.getElementById("valAguaAp102");
+    var acrescAgua102 =  document.getElementById("acrescAgua102");
+    var totalAgua102 =  document.getElementById("totalAgua102");
+
+    var gasMesAnt102 = document.getElementById("gasMesAnt102");
+    var gasMesAtu102 = document.getElementById("gasMesAtu102");
+    var mcgas102 =  document.getElementById("mcgas102");
+    var totalGas102 =  document.getElementById("totalGas102");
+
+    var mAnt102 = prompt("leitura mes Anterior Agua 102: ");
+    aguaMesAnt102.innerText =  mAnt102;
+
+    var mAtu102 = prompt("Informe leitura mes Atual Agua 102: ");
+    aguaMesAtu102.innerText +=   mAtu102;
+
+    var mc102 = prompt("Mc Agua 102: ");
+    mcagua102.innerText = mc102;
+
+    var valAgua102 = prompt("Valor Gasto 102: ");
+    valAguaAp102.innerText = "R$ " + valAgua102;
+
+    var acrescimo102 = prompt("Acrescimo: ");
+    acrescAgua102.innerText = "R$ " + acrescimo102;
+
+    var totA102 = prompt("Total de Agua 102: ");
+    totalAgua102.innerText = "R$ " + totA102;
+
+    var gasMAnt102 = prompt("Leitura anterior gas 102: ");
+    gasMesAnt102.innerText = gasMAnt102;
+
+    var gasMAtu102 = prompt("Leitura atual gas 102: ")
+    gasMesAtu102.innerText = gasMAtu102;
+
+    var gasM102 = prompt("Mc Gas 102: ");
+    mcgas102.innerText = gasM102;
+
+    var totG102 = prompt("Informe Total de gas 102: ");
+    totalGas102.innerText = "R$ " + totG102;
+};
+
+function ap201() {
+    var aguaMesAnt103 = document.getElementById("aguaMesAnt103");
+    var AguaMesAtu103 =  document.getElementById("AguaMesAtu103");
+    var mcagua103 =  document.getElementById("mcagua103");
+    var valAguaAp103 = document.getElementById("valAguaAp103");
+    var acrescAgua103 =  document.getElementById("acrescAgua103");
+    var totalAgua103 =  document.getElementById("totalAgua103");
+
+    var gasMesAnt103 = document.getElementById("gasMesAnt103");
+    var gasMesAtu103 = document.getElementById("gasMesAtu103");
+    var mcgas103 =  document.getElementById("mcgas103");
+    var totalGas103 =  document.getElementById("totalGas103");
+
+    var mAnt103 = prompt("leitura mes Anterior Agua 201: ");
+    aguaMesAnt103.innerText =  mAnt103;
+
+    var mAtu103 = prompt("Informe leitura mes Atual Agua 201: ");
+    aguaMesAtu103.innerText +=   mAtu103;
+
+    var mc103 = prompt("Mc Agua 201: ");
+    mcagua103.innerText = mc103;
+
+    var valAgua103 = prompt("Valor Gasto 201: ");
+    valAguaAp103.innerText = "R$ " + valAgua103;
+
+    var acrescimo103 = prompt("Acrescimo: ");
+    acrescAgua103.innerText = "R$ " + acrescimo103;
+
+    var totA103 = prompt("Total de Agua 201: ");
+    totalAgua103.innerText = "R$ " + totA103;
+
+    var gasMAnt103 = prompt("Leitura anterior gas 201: ");
+    gasMesAnt103.innerText = gasMAnt103;
+
+    var gasMAtu103 = prompt("Leitura atual gas 201: ")
+    gasMesAtu103.innerText = gasMAtu103;
+
+    var gasM103 = prompt("Mc Gas 201: ");
+    mcgas103.innerText = gasM103;
+
+    var totG103 = prompt("Informe Total de gas 201: ");
+    totalGas103.innerText = "R$ " + totG103;
+};
+
+function ap202() {
+    var aguaMesAnt104 = document.getElementById("aguaMesAnt104");
+    var AguaMesAtu104 =  document.getElementById("AguaMesAtu104");
+    var mcagua104 =  document.getElementById("mcagua104");
+    var valAguaAp104 = document.getElementById("valAguaAp104");
+    var acrescAgua104 =  document.getElementById("acrescAgua104");
+    var totalAgua104 =  document.getElementById("totalAgua104");
+
+    var gasMesAnt104 = document.getElementById("gasMesAnt104");
+    var gasMesAtu104 = document.getElementById("gasMesAtu104");
+    var mcgas104 =  document.getElementById("mcgas104");
+    var totalGas104 =  document.getElementById("totalGas104");
+
+    var mAnt104 = prompt("leitura mes Anterior Agua 202:  ");
+    aguaMesAnt104.innerText =  mAnt104;
+
+    var mAtu104 = prompt("Informe leitura mes Atual Agua 202: : ");
+    aguaMesAtu104.innerText +=   mAtu104;
+
+    var mc104 = prompt("Mc Agua 202: ");
+    mcagua104.innerText = mc104;
+
+    var valAgua104 = prompt("Valor Gasto 202: ");
+    valAguaAp104.innerText = "R$ " + valAgua104;
+
+    var acrescimo104 = prompt("Acrescimo: ");
+    acrescAgua104.innerText = "R$ " + acrescimo104;
+
+    var totA104 = prompt("Total de Agua 202: ");
+    totalAgua104.innerText = "R$ " + totA104;
+
+    var gasMAnt104 = prompt("Leitura anterior gas 202: ");
+    gasMesAnt104.innerText = gasMAnt104;
+
+    var gasMAtu104 = prompt("Leitura atual gas 202: ")
+    gasMesAtu104.innerText = gasMAtu104;
+
+    var gasM104 = prompt("Mc Gas 202: ");
+    mcgas104.innerText = gasM104;
+
+    var totG104 = prompt("Informe Total de gas 202: ");
+    totalGas104.innerText = "R$ " + totG104;
+};
+
+function ap301() {
+    var aguaMesAnt105 = document.getElementById("aguaMesAnt105");
+    var AguaMesAtu105 =  document.getElementById("AguaMesAtu105");
+    var mcagua105 =  document.getElementById("mcagua105");
+    var valAguaAp105 = document.getElementById("valAguaAp105");
+    var acrescAgua105 =  document.getElementById("acrescAgua105");
+    var totalAgua105 =  document.getElementById("totalAgua105");
+
+    var gasMesAnt105 = document.getElementById("gasMesAnt105");
+    var gasMesAtu105 = document.getElementById("gasMesAtu105");
+    var mcgas105 =  document.getElementById("mcgas105");
+    var totalGas105 =  document.getElementById("totalGas105");
+
+    var mAnt105 = prompt("leitura mes Anterior Agua 301: ");
+    aguaMesAnt105.innerText =  mAnt105;
+
+    var mAtu105 = prompt("Informe leitura mes Atual Agua 301: ");
+    aguaMesAtu105.innerText +=   mAtu105;
+
+    var mc105 = prompt("Mc Agua 301: ");
+    mcagua105.innerText = mc105;
+
+    var valAgua105 = prompt("Valor Gasto 301: ");
+    valAguaAp105.innerText = "R$ " + valAgua105;
+
+    var acrescimo105 = prompt("Acrescimo: ");
+    acrescAgua105.innerText = "R$ " + acrescimo105;
+
+    var totA105 = prompt("Total de Agua 301: ");
+    totalAgua105.innerText = "R$ " + totA105;
+
+    var gasMAnt105 = prompt("Leitura anterior gas 301: ");
+    gasMesAnt105.innerText = gasMAnt105;
+
+    var gasMAtu105 = prompt("Leitura atual gas 301: ")
+    gasMesAtu105.innerText = gasMAtu105;
+
+    var gasM105 = prompt("Mc Gas 301: ");
+    mcgas105.innerText = gasM105;
+
+    var totG105 = prompt("Informe Total de gas 301: ");
+    totalGas105.innerText = "R$ " + totG105;
+};
+
+function ap302() {
+    var aguaMesAnt106= document.getElementById("aguaMesAnt106");
+    var AguaMesAtu106 =  document.getElementById("AguaMesAtu106");
+    var mcagua106 =  document.getElementById("mcagua106");
+    var valAguaAp106 = document.getElementById("valAguaAp106");
+    var acrescAgua106 =  document.getElementById("acrescAgua106");
+    var totalAgua106 =  document.getElementById("totalAgua106");
+
+    var gasMesAnt106 = document.getElementById("gasMesAnt106");
+    var gasMesAtu106 = document.getElementById("gasMesAtu106");
+    var mcgas106 =  document.getElementById("mcgas106");
+    var totalGas106 =  document.getElementById("totalGas106");
+
+    var mAnt106 = prompt("leitura mes Anterior Agua 302: ");
+    aguaMesAnt106.innerText =  mAnt106;
+
+    var mAtu106 = prompt("Informe leitura mes Atual Agua 302: ");
+    aguaMesAtu106.innerText +=   mAtu106;
+
+    var mc106 = prompt("Mc Agua 302: ");
+    mcagua106.innerText = mc106;
+
+    var valAgua106 = prompt("Valor Gasto 302: ");
+    valAguaAp106.innerText = "R$ " + valAgua106;
+
+    var acrescimo106 = prompt("Acrescimo: ");
+    acrescAgua106.innerText = "R$ " + acrescimo106;
+
+    var totA106 = prompt("Total de Agua 302: ");
+    totalAgua106.innerText = "R$ " + totA106;
+
+    var gasMAnt106 = prompt("Leitura anterior gas 302: ");
+    gasMesAnt106.innerText = gasMAnt106;
+
+    var gasMAtu106 = prompt("Leitura atual gas 302: ")
+    gasMesAtu106.innerText = gasMAtu106;
+
+    var gasM106 = prompt("Mc Gas 302: ");
+    mcgas106.innerText = gasM106;
+
+    var totG106 = prompt("Informe Total de gas 302: ");
+    totalGas106.innerText = "R$ " + totG106;
+};
